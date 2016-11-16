00f4a391c6f9a698bbcbbd05bf3d80e213c82884
Yifan Hong <elsk@google.com>
2016-11-16 12:35:58 -0800

    Split libhidl into base and transport.
    libhidlbase contains "pure" classes that has nothing
    to do with tranport level stuff. libhwbinder can now
    safely depend on libhidlbase.
    
    IBinder inheriting from IBase will be in a follow up
    CL.
    
    Bug: 32756130
    
    Test: mma under hardware/interfaces
    Test: hidl_test
    Change-Id: Ie2bd80769df3816aab83789ba0d7fca9b5a2e52f

--- manager/1.0/update-makefiles.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ! -d system/libhidl ] ; then
-  echo "Where is system/libhidl?";
-  exit 1;
-fi
-
-hidl-gen -Lmakefile -r android.hidl:system/libhidl android.hidl.manager@1.0
-hidl-gen -Landroidbp -r android.hidl:system/libhidl android.hidl.manager@1.0
