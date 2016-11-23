a8b3baf5835634f0a36db185bd696c5786b67813
Yifan Hong <elsk@google.com>
2016-11-23 11:30:36 -0800

    Hidlize IBase and use IBase instead of IBinder.
    Note: system/libhidl/base is the libhidlbase project.
    system/libhidl/transport/base is the
    android.hidl.base@1.0 HAL files.
    
    Test: hidl_test
    
    Change-Id: Ic3464b5e50c0896de9b26f44a409496f8df37c39

--- update-makefiles.sh
+++ update-makefiles.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # TODO(b/33276472)
-
-if [ ! -d system/libhidl ] ; then
-  echo "Where is system/libhidl?";
+if [ ! -d system/libhidl/transport ] ; then
+  echo "Where is system/libhidl/transport?";
   exit 1;
 fi
+
+#base
+hidl-gen -Lmakefile -r android.hidl:system/libhidl/transport android.hidl.base@1.0
+hidl-gen -Landroidbp -r android.hidl:system/libhidl/transport android.hidl.base@1.0
 
 #manager
 hidl-gen -Lmakefile -r android.hidl:system/libhidl/transport android.hidl.manager@1.0
